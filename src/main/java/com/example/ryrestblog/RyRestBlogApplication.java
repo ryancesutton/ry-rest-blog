3b461e482b494a24bd34519dd1dacbcc591b7623
Ry Sutton <ryancesutton@gmail.com>
2021-09-12 22:30:03 -0500

    initial commit

---
+++ src/main/java/com/example/ryrestblog/RyRestBlogApplication.java
@@ -0,0 +1,13 @@
+package com.example.ryrestblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RyRestBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RyRestBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ryrestblog/RyRestBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ryrestblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RyRestBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
